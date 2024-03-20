d8cecfa2be8c41287e9a47a864468f50534c7a2a
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-20 17:10:23 +0600

    added hero section

--- components/home-page/Hero.tsx
+++ components/home-page/Hero.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import Image from "next/image";
+import Classes from "./hero.module.css";
 
 const Hero = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className={Classes.hero}>
+      <div className={Classes.image}>
+        <Image src="/images/site/rakib.jpg" alt="An image showing Rakib" width={800} height={800} />
+         </div>
+      <h1>Hi, I&apos;m Rakib</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like React
+      </p>
+    </section>
+  );
 };
 
 export default Hero;
