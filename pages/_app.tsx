e90fc9477c102b79924b4378635863a119c55819
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-20 22:27:13 +0600

    added meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Next Blog is a blog website that is built with NextJS by Rakib. It is a blog about web development - especially frontend frameworks like React."
+        />
+        <title>Next Blog</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
