4ab1db02601e2784e7a15501821e0f9200871384
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-20 22:54:09 +0600

    homepage completed

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -18,12 +18,19 @@ const PostItem = ({ title, image, excerpt, date, slug }: PostItemProps) => {
   });
 
   const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   return (
     <li className={classes.post}>
-      <Link href="/" passHref>
+      <Link href={linkPath} passHref>
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200} />
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            layout="responsive"
+          />
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,11 @@ export default function App({ Component, pageProps }: AppProps) {
     <Layout>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Abdullah Al Rakib"></meta>
         <meta
           name="description"
-          content="Next Blog is a blog website that is built with NextJS by Rakib. It is a blog about web development - especially frontend frameworks like React."
+          content="Next Blog is a blog website that is built with NextJS by Abdullah Al Rakib. It is a blog about web development - especially frontend frameworks like React."
+          key={"about-next-blog-meta"}
         />
         <title>Next Blog</title>
       </Head>
