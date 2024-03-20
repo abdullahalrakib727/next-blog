4cd89e15214f53bcb4b294f4acbad7c29720dffa
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-20 17:15:41 +0600

    added hero image

--- components/home-page/Hero.tsx
+++ components/home-page/Hero.tsx
@@ -5,8 +5,14 @@ const Hero = () => {
   return (
     <section className={Classes.hero}>
       <div className={Classes.image}>
-        <Image src="/images/site/rakib.jpg" alt="An image showing Rakib" width={800} height={800} />
-         </div>
+        <Image
+          src="/images/site/rakib.jpg"
+          alt="An image showing Rakib"
+          width={800}
+          height={800}
+          priority={true}
+        />
+      </div>
       <h1>Hi, I&apos;m Rakib</h1>
       <p>
         I blog about web development - especially frontend frameworks like React
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import Hero from "@/components/home-page/Hero";
+import Head from "next/head";
 
 const HomePage = () => {
   return (
     <main>
+      <Head>
+        <meta
+          name="description"
+          content="I am Rakib. I blog about web development - especially frontend frameworks like React"
+        />
+        <title>Next Blog</title>
+      </Head>
       <Hero />
     </main>
   );
