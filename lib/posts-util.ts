15b8647302970ecdac7cba29c032b4a2a036c001
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-22 16:36:56 +0600

    added post details

--- lib/posts-util.ts
+++ lib/posts-util.ts
@@ -14,12 +14,15 @@ type PostData = {
   isFeatured?: boolean;
 };
 
-const getPostData = (fileName: string) => {
-  const filePath = path.join(postsDirectory, fileName);
+export const getPostsFiles = () => {
+  return fs.readdirSync(postsDirectory);
+};
+
+export const getPostData = (fileName: string) => {
+  const postSlug = fileName.replace(/\.md$/, ""); // removes the file extension
+  const filePath = path.join(postsDirectory, `${postSlug}.md`);
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const { data, content } = matter(fileContent);
-
-  const postSlug = fileName.replace(/\.md$/, ""); // removes the file extension
 
   const postData: PostData = {
     slug: postSlug,
@@ -31,7 +34,7 @@ const getPostData = (fileName: string) => {
 };
 
 export const getAllPosts = () => {
-  const postFiles = fs.readdirSync(postsDirectory);
+  const postFiles = getPostsFiles();
 
   const allPosts = postFiles.map((postFile) => {
     return getPostData(postFile);
@@ -47,5 +50,3 @@ export const getFeaturedPosts = () => {
 
   return featurePosts;
 };
-
-
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,8 +1,42 @@
 import PostContent from "@/components/posts/post-detail/PostContent";
-import React from "react";
+import { getPostData, getPostsFiles } from "@/lib/posts-util";
+import { GetStaticPaths, GetStaticProps } from "next";
 
-const PostDetailsPage = () => {
-  return <PostContent />;
+export type PostDetailProps = {
+  post: {
+    slug: string;
+    title: string;
+    image: string;
+    date: string;
+    content: string;
+  };
+};
+
+const PostDetailsPage = ({ post }: PostDetailProps) => {
+  return <PostContent post={post} />;
 };
 
 export default PostDetailsPage;
+
+export const getStaticProps: GetStaticProps = (ctx) => {
+  const { params } = ctx;
+  const slug = params?.slug as string;
+
+  const data = getPostData(slug);
+
+  return {
+    props: {
+      post: data,
+    },
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const postFilenames = getPostsFiles();
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
+};
