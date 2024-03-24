d8f0b58bb3cbe1625e66d9abde0475c803572b74
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-24 11:46:56 +0600

    added contact form

---
+++ components/contact/ContactForm.tsx
@@ -0,0 +1,28 @@
+import classes from "./contact-form.module.css";
+
+const ContactForm = () => {
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you ?</h1>
+      <form className={classes.contact}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="name">Your Name</label>
+          <input type="text" id="name" required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="message">Your Message</label>
+          <textarea id="message" rows={5} required></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default ContactForm;
