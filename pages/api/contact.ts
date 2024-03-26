e00f57d61a97551dc625d2eaa3f25f1b28cfa845
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-03-26 21:00:26 +0600

    added local mongodb conncection for contact page

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,7 @@
+import { MongoClient } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, name, message } = req.body;
   if (req.method === "POST") {
     if (
@@ -20,8 +21,26 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
       message,
     };
 
-    console.log(newMessage);
-    res.status(201).json({ message: "Successfully stored message!" });
+    const url = "mongodb://localhost:27017/messagesDb";
+
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+    } catch (error) {
+      return res.status(500).json({ message: "Could not connect to database" });
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+
+      return res
+        .status(201)
+        .json({ message: "Successfully stored message!", result });
+    } catch (error) {
+      return res.status(500).json({ message: "Could not store message" });
+    }
   }
 };
 
